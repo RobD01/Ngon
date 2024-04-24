3c7d32cac4662f63f75fc366b1c0e9304c10657c
RobD01 <medbz2k3@gmail.com>
2024-04-23 23:21:38 -0700

    github pages

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { Routes, Route } from "react-router-dom";
+import "./globals.css";
+import SignInForm from "./_auth/forms/SignInForm";
+import {
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Explore,
+  Home,
+  LikedPosts,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "./_root/pages";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/pages/RootLayout";
+import { Toaster } from "./components/ui/toaster";
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+        {/* private */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
+          <Route path="/update-profile/:id" element={<LikedPosts />} />
+        </Route>
+      </Routes>
+      <Toaster />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,26 @@
+import { Outlet, Navigate } from "react-router-dom";
+
+const AuthLayout = () => {
+  const isAuthenticated = false;
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 items-center flex-col py-10">
+            <Outlet />
+          </section>
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className=" md:block h-screen w-1/2 object-cover bg-no=repeat sm:hidden"
+          />
+        </>
+      )}
+    </>
+  );
+};
+
+export default AuthLayout;
