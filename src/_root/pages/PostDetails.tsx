f802726b98715656b0b74262a2051386ec329a08
RobD01 <medbz2k3@gmail.com>
2024-05-03 23:13:34 -0700

    post details edit

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,5 +1,6 @@
 import Loader from "@/components/shared/Loader";
 import { baseUrl } from "@/constants";
+import { useUserContext } from "@/context/AuthContext";
 import { useGetPostById } from "@/lib/react-query/queriesAndMutations";
 import { multiFormatDateString } from "@/lib/utils";
 import { Link, useParams } from "react-router-dom";
@@ -7,8 +8,7 @@ import { Link, useParams } from "react-router-dom";
 const PostDetails = () => {
   const { id } = useParams();
   const { data: post, isPending } = useGetPostById(id || "");
-
-  console.log(post);
+  const { user } = useUserContext();
 
   return (
     <div className="post_details-container">
@@ -17,28 +17,48 @@ const PostDetails = () => {
           <Loader /> Loading ...
         </>
       )}
-      <div className="post-card">
-        <div className="flex items-center gap-3">
-          <Link to={`/profile/${post?.creator.$id}`}>
-            <img
-              src={
-                post?.creator?.imageUrl ||
-                baseUrl + "/assets/icons/profile-placeholder.svg"
-              }
-              className="rounded-full w-12 lg:h-12"
-            />
-          </Link>
-          <div className="flex flex-col lg:gap-2">
-            <Link to={`/profile/${post?.creator.$id}`}>
-              <p className="base-medium lg:body-bold">{post?.creator.name}</p>
-            </Link>
+      <div className="post_details-card">
+        <img src={post?.imageUrl} alt="post" className="post_details-img" />
+        <div className="post_details-info">
+          <div className="flex-between w-full">
+            <section className="flex items-center gap-3">
+              <Link to={`/profile/${post?.creator.$id}`}>
+                <img
+                  src={
+                    post?.creator?.imageUrl ||
+                    baseUrl + "/assets/icons/profile-placeholder.svg"
+                  }
+                  className="rounded-full size-8 lg:size-12"
+                />
+              </Link>
+              <div className="flex flex-col lg:gap-2">
+                <Link to={`/profile/${post?.creator.$id}`}>
+                  <p className="base-medium lg:body-bold">
+                    {post?.creator.name}
+                  </p>
+                </Link>
 
-            <p className="subtle-semibold lg:small-regular">
-              {multiFormatDateString(post?.$createdAt)}
-            </p>
-            <p className="subtle-semibold lg:small-regular">
-              Location: {post?.location}
-            </p>
+                <p className="subtle-semibold lg:small-regular">
+                  {multiFormatDateString(post?.$createdAt)}
+                </p>
+                <p className="subtle-semibold lg:small-regular">
+                  Location: {post?.location}
+                </p>
+              </div>
+            </section>
+            <section className="flex-center gap-4">
+              <Link
+                to={`/update-post/${post?.$id}`}
+                className={`${user.id !== post?.creator.$id && "hidden"}`}
+              >
+                <img
+                  src={baseUrl + "/assets/icons/edit.svg"}
+                  alt="edit"
+                  width={24}
+                  height={24}
+                />
+              </Link>
+            </section>
           </div>
         </div>
       </div>
