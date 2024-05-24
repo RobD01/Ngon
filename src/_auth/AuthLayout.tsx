1d4f27156483a2cf076ade592f6e0259b115683e
RobD01 <medbz2k3@gmail.com>
2024-05-23 23:10:09 -0700

    auth layout/ user context

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -3,7 +3,7 @@ import { baseUrl } from "@/constants";
 import { useUserContext } from "@/context/AuthContext";
 
 const AuthLayout = () => {
-  const isAuthenticated = false;
+  const isAuthenticated = useUserContext().isAuthenticated;
 
   return (
     <>
--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,16 +1,16 @@
-import GridPostList from "@/components/shared/GridPostList";
-import SearchResults from "@/components/shared/SearchResults";
-import { Input } from "@/components/ui/input";
-import { baseUrl } from "@/constants";
-import { useState } from "react";
+// import GridPostList from "@/components/shared/GridPostList";
+// import SearchResults from "@/components/shared/SearchResults";
+// import { Input } from "@/components/ui/input";
+// import { baseUrl } from "@/constants";
+// import { useState } from "react";
 
 const Explore = () => {
-  const [searchValue, setSearchValue] = useState("");
-  const posts = [];
-  const shouldShowSearchResults = searchValue !== "";
-  const shouldShowPosts =
-    !shouldShowSearchResults &&
-    posts.pages.every((item) => item.documents.length === 0);
+  // const [searchValue, setSearchValue] = useState("");
+  // const posts = [pages];
+  // const shouldShowSearchResults = searchValue !== "";
+  // const shouldShowPosts =
+  //   !shouldShowSearchResults &&
+  //   posts.pages.every((item) => item.documents.length === 0);
 
   return (
     <div className="explore-container">
@@ -18,7 +18,7 @@ const Explore = () => {
         <h2 className="h3-bold md:h2-bold w-full"> Search Posts</h2>
 
         {/* search */}
-        <section className="flex gap-1 px-4 py-1 w-full rounded-lg bg-light-1 shadow-md">
+        {/* <section className="flex gap-1 px-4 py-1 w-full rounded-lg bg-light-1 shadow-md">
           <img
             src={baseUrl + "/assets/icons/search.svg"}
             alt="search"
@@ -32,11 +32,11 @@ const Explore = () => {
             value={searchValue}
             onChange={(e) => setSearchValue(e.target.value)}
           />
-        </section>
+        </section> */}
       </div>
 
       {/* explore */}
-      <section className="flex-between w-full max-w-5xl mt-8 md:mt-16  mb-7">
+      {/* <section className="flex-between w-full max-w-5xl mt-8 md:mt-16  mb-7">
         <h2 className="body-bold md:h3-bold">Popular Today</h2>
         <div className="search-filter ">
           <p className="small-medium md:base-medium">All</p>
@@ -47,10 +47,10 @@ const Explore = () => {
             alt="filter"
           />
         </div>
-      </section>
+      </section> */}
 
       {/* Search Results */}
-      <section className="flex flex-wrap gap-9 w-full max-w-5xl">
+      {/* <section className="flex flex-wrap gap-9 w-full max-w-5xl">
         {shouldShowSearchResults ? (
           <SearchResults />
         ) : shouldShowPosts ? (
@@ -60,7 +60,7 @@ const Explore = () => {
             <GridPostList key={`page-${index}`} posts={item.documents} />
           ))
         )}
-      </section>
+      </section> */}
     </div>
   );
 };
