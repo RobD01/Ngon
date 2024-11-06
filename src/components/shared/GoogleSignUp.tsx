95c1a5f61ac137f76c1ca450089706e1c6279f6f
RobD01 <medbz2k3@gmail.com>
2024-11-05 22:15:03 -0800

    google oauth fail url

--- src/components/shared/GoogleSignUp.tsx
+++ src/components/shared/GoogleSignUp.tsx
@@ -12,7 +12,7 @@ export const GoogleSignUp = () => {
       account.createOAuth2Session(
         "google",
         "http://ngonapp.com/",
-        "http:////ngonapp.com/#/sign-up"
+        "*"
       );
     } catch (error) {
       console.log(error);
